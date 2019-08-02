d52e23e4cf22a2b0fa622d42b77b7ffd00df8b39
RathaSame <rathasame12@gmail.com>
2019-08-02 21:59:00 +0700

    update

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use App\Models\Commune;
 use App\Models\District;
+use App\Models\Province;
+use App\Models\StaffDesignation;
 use Illuminate\Database\Eloquent\Model;
 
 class Staff extends BaseModel
 {
     protected $fillable = ['created_by', 'last_updated_by', 'reg_no', 'join_date', 'designation', 'first_name',  'middle_name', 'last_name',
         'father_name', 'mother_name', 'date_of_birth', 'gender', 'blood_group', 'nationality','mother_tongue', 'address', 'state', 'country',
-        'temp_address', 'temp_state', 'temp_country', 'home_phone', 'mobile_1', 'mobile_2', 'email', 'qualification',
+        'temp_address', 'temp_state', 'temp_country','temp_village', 'home_phone', 'mobile_1', 'mobile_2', 'email', 'qualification',
         'experience', 'experience_info', 'other_info','staff_image','institute_id','province_id','district_id','commune_id'];
 
     public function payrollMaster()
@@ -36,6 +38,10 @@ class Staff extends BaseModel
     public function communes()   
     {
         return $this->belongsTo(Commune::class, 'commune_id');
+    }
+    public function designat()   
+    {
+        return $this->belongsTo(StaffDesignation::class, 'designation');
     }
 
 }
