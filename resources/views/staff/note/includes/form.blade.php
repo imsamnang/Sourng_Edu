bb737c0b73901ed945f55fa53302f274fe72f507
Sourng Edu <sourngedu@gmail.com>
2019-08-02 23:10:42 +0700

    update Staff
    Update Staff and Student Search

--- resources/views/staff/note/includes/form.blade.php
+++ resources/views/staff/note/includes/form.blade.php
@@ -1,21 +1,21 @@
 <div class="form-group">
-    {!! Form::label('reg_no', __('staff_frm_note_Reg_No'), ['class' => 'col-sm-3 control-label']) !!}
+    {!! Form::label('reg_no', __('staff_frm_note_Reg_No'), ['class' => 'col-sm-4 control-label']) !!}
     <div class="col-sm-8">
-        {!! Form::text('reg_no', null, ["placeholder" => "", "class" => "form-control border-form input-mask-registration","required"]) !!}
+        {!! Form::text('reg_no', null, ["placeholder" => "Staff Register No", "class" => "form-control border-form input-mask-registration","required"]) !!}
         @include('includes.form_fields_validation_message', ['name' => 'reg_no'])
     </div>
 
 </div>
 <div class="form-group">
-    {!! Form::label('subject', __('staff_frm_note_Subject'), ['class' => 'col-sm-2 control-label']) !!}
-    <div class="col-sm-9">
+    {!! Form::label('subject', __('staff_frm_note_Subject'), ['class' => 'col-sm-4 control-label']) !!}
+    <div class="col-sm-12">
         {!! Form::text('subject', null, ["placeholder" => "", "class" => "form-control border-form","required"]) !!}
         @include('includes.form_fields_validation_message', ['name' => 'subject'])
     </div>
 </div>
 <div class="form-group">
-    {!! Form::label('note', __('staff_frm_note_Note'), ['class' => 'col-sm-2 control-label']) !!}
-    <div class="col-sm-10">
+    {!! Form::label('note', __('staff_frm_note_Note'), ['class' => 'col-sm-4 control-label']) !!}
+    <div class="col-sm-12">
         {!! Form::textarea('note', null, ["placeholder" => "", "class" => "form-control border-form", "rows"=>"3"]) !!}
         @include('includes.form_fields_validation_message', ['name' => 'note'])
     </div>
--- resources/views/student/includes/form.blade.php
+++ resources/views/student/includes/form.blade.php
@@ -1,56 +1,77 @@
-    <h4 class="header large lighter blue"><i class="fa fa-search" aria-hidden="true"></i>&nbsp;@lang('m_Detail_Search_Student')</h4>
-    <div class="clearfix">
-        <div class="form-group">
-            {!! Form::label('reg_no', __('m_stu_detail.regno'), ['class' => 'col-sm-2 control-label']) !!}
-            <div class="col-sm-3">
-                {!! Form::text('reg_no', null, ["placeholder" => "", "class" => "form-control border-form input-mask-registration", "autofocus"]) !!}
-                @include('includes.form_fields_validation_message', ['name' => 'reg_no'])
-            </div>
 
-            {!! Form::label('reg_date', __('m_stu_detail_regdate'), ['class' => 'col-sm-2 control-label']) !!}
-            <div class=" col-sm-5">
-                <div class="input-group ">
-                    {!! Form::text('reg_start_date', null, ["placeholder" => "YYYY-MM-DD", "class" => "input-sm form-control border-form input-mask-date date-picker", "data-date-format" => "yyyy-mm-dd"]) !!}
-                    <span class="input-group-addon">
-                        <i class="fa fa-exchange"></i>
-                    </span>
-                    {!! Form::text('reg_end_date', null, ["placeholder" => "YYYY-MM-DD", "class" => "input-sm form-control border-form input-mask-date date-picker", "data-date-format" => "yyyy-mm-dd"]) !!}
-                    @include('includes.form_fields_validation_message', ['name' => 'reg_start_date'])
-                    @include('includes.form_fields_validation_message', ['name' => 'reg_end_date'])
+  
+<div class="panel panel-default">
+        <div class="panel-heading">
+            <a href="#faq-1-1" data-parent="#faq-list-1" data-toggle="collapse" class="accordion-toggle collapsed">
+                <i class="ace-icon fa fa-chevron-left pull-right" data-icon-hide="ace-icon fa fa-chevron-down" data-icon-show="ace-icon fa fa-chevron-left"></i>
+    
+                <h4 data-parent="#faq-list-1" data-toggle="collapse" class="accordion-toggle collapsed" class="header large lighter blue"><i class="fa fa-search" aria-hidden="true"></i>&nbsp;@lang('m_Detail_Search_Student')</h4>
+            </a>
+        </div>
+    
+        <div class="panel-collapse collapse" id="faq-1-1">
+            <div class="panel-body">
+                    <div class="clearfix">
+                            <div class="form-group">
+                                {!! Form::label('reg_no', __('m_stu_detail.regno'), ['class' => 'col-sm-2 control-label']) !!}
+                                <div class="col-sm-3">
+                                    {!! Form::text('reg_no', null, ["placeholder" => "", "class" => "form-control border-form input-mask-registration", "autofocus"]) !!}
+                                    @include('includes.form_fields_validation_message', ['name' => 'reg_no'])
+                                </div>
+
+                                {!! Form::label('reg_date', __('m_stu_detail_regdate'), ['class' => 'col-sm-1 control-label']) !!}
+                                <div class=" col-sm-5">
+                                    <div class="input-group ">
+                                        {!! Form::text('reg_start_date', null, ["placeholder" => "YYYY-MM-DD", "class" => "input-sm form-control border-form input-mask-date date-picker", "data-date-format" => "yyyy-mm-dd"]) !!}
+                                        <span class="input-group-addon">
+                                            <i class="fa fa-exchange"></i>
+                                        </span>
+                                        {!! Form::text('reg_end_date', null, ["placeholder" => "YYYY-MM-DD", "class" => "input-sm form-control border-form input-mask-date date-picker", "data-date-format" => "yyyy-mm-dd"]) !!}
+                                        @include('includes.form_fields_validation_message', ['name' => 'reg_start_date'])
+                                        @include('includes.form_fields_validation_message', ['name' => 'reg_end_date'])
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">@lang('m_stu_detail_faculty')</label>
+                                <div class="col-sm-3">
+                                    {!! Form::select('faculty', $data['faculties'], null, ['class' => 'form-control', 'onChange' => 'loadSemesters(this);']) !!}
+
+                                </div>
+
+                            <label class="col-sm-1 control-label">@lang('m_stu_detail_semester')</label>
+                            <div class="col-sm-2">
+                                <select name="semester_select[]" class="form-control semester_select" >
+                                    <option> @lang('m_stu_Detail_Select_Semester') </option>
+                                </select>
+                            </div>
+
+                            <label class="col-sm-1 control-label">@lang('m_stu_detail_status')</label>
+                            <div class="col-sm-2">
+                                <select class="form-control border-form" name="status" id="cat_id">
+                                    <option value="all"> @lang('m_stu_Detail_select_status') </option>
+                                    <option value="active" >@lang('m_stu_detail_active')</option>
+                                    <option value="in-active" >@lang('m_stu_detail_in_active')</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+
+                <div class="clearfix form-actions">
+                    <div class="col-md-12 align-right">        &nbsp; &nbsp; &nbsp;
+                        <button class="btn btn-info" type="submit" id="filter-btn">
+                            <i class="fa fa-filter bigger-110"></i>
+                            @lang('frm_Search')
+                        </button>
+                    </div>
                 </div>
+
+
+
+                
             </div>
         </div>
-
-        <div class="form-group">
-            <label class="col-sm-2 control-label">@lang('m_stu_detail_faculty')</label>
-            <div class="col-sm-3">
-                {!! Form::select('faculty', $data['faculties'], null, ['class' => 'form-control', 'onChange' => 'loadSemesters(this);']) !!}
-
-            </div>
-
-        <label class="col-sm-2 control-label">@lang('m_stu_detail_semester')</label>
-        <div class="col-sm-2">
-            <select name="semester_select[]" class="form-control semester_select" >
-                <option> @lang('m_stu_Detail_Select_Semester') </option>
-            </select>
-        </div>
-
-        <label class="col-sm-1 control-label">@lang('m_stu_detail_status')</label>
-        <div class="col-sm-2">
-            <select class="form-control border-form" name="status" id="cat_id">
-                <option value="all"> @lang('m_stu_Detail_select_status') </option>
-                <option value="active" >@lang('m_stu_detail_active')</option>
-                <option value="in-active" >@lang('m_stu_detail_in_active')</option>
-            </select>
-        </div>
     </div>
-</div>
-<div class="clearfix form-actions">
-    <div class="col-md-12 align-right">        &nbsp; &nbsp; &nbsp;
-        <button class="btn btn-info" type="submit" id="filter-btn">
-            <i class="fa fa-filter bigger-110"></i>
-            @lang('frm_Search')
-        </button>
-    </div>
-</div>
 
