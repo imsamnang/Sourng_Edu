fac18747a4d17d87fb28168df40842b6d54e4535
Sourng Edu <sourngedu@gmail.com>
2019-10-08 20:21:58 +0700

    Update Report

--- app/Http/Controllers/Report/StaffReportController.php
+++ app/Http/Controllers/Report/StaffReportController.php
@@ -26,7 +26,7 @@ class StaffReportController extends CollegeBaseController
         $data = [];
         $data['staff'] = Staff::select('id','reg_no', 'join_date', 'designation', 'first_name',  'middle_name', 'last_name',
             'father_name', 'mother_name', 'date_of_birth', 'gender', 'blood_group', 'nationality','mother_tongue', 'address', 'state', 'country',
-            'temp_address', 'temp_state', 'temp_country', 'home_phone', 'mobile_1', 'mobile_2', 'email', 'qualification',
+            'temp_address', 'temp_state', 'temp_country', 'home_phone', 'mobile_1', 'mobile_2', 'email', 'qualification_id','qualification',
             'experience', 'experience_info', 'other_info','status')
             ->where(function ($query) use ($request) {
 
